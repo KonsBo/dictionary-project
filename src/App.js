3f6ee55d9cbddcc4923c6fabd902ba05ee11b9d4
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-08-07 22:28:21 +0300

    Changes in styling

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid " alt="logo" />
+          <div class="icons d-flex d-md-block" title="Boua's Logo">
+            <span class="icon-right">﹆</span>
+            <span class="icon-middle">﹆</span>
+            <span class="icon-left">﹆</span>
+            <span class="icon-bottom">﹆</span>
+          </div>
         </header>
         <main>
           <Dictionary defaultKeyword="yoga" />
         </main>
         <footer className="App-footer">
           <div className="footer">
-            <img src={logo} className="footer-logo img-fluid " alt="logo" />
+            {/* <img src={logo} className="footer-logo img-fluid " alt="logo" /> */}
+            <img
+              src={logo}
+              className="App-logo footer-logo img-fluid "
+              alt="logo"
+            />
             <small>
-              Coded by <em>KonBo</em> | open-sourced on{" "}
+              Coded by <em>KonBo</em> open-sourced on{" "}
               <a href="https://github.com/KonsBo/dictionary-project">Github</a>{" "}
-              | and hosted on{" "}
+              and hosted on{" "}
               <a href="https://app-dictionary-project.netlify.app/">Netlify</a>
             </small>
           </div>
